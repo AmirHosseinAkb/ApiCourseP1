7c3a03248431e4e16c11849831ac1aa32b38e083
unknown <akbariamirhosseinx@gmail.com>
2023-10-26 12:39:17 +0330

    Entities Added

---
+++ Entities/User/Role.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class Role : BaseEntity
+    {
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; }
+    }
+}
