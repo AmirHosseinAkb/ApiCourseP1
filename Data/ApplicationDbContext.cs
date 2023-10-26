4ceaacb2979ce2ad74ed697c719d65ce720cb088
unknown <akbariamirhosseinx@gmail.com>
2023-10-26 13:07:51 +0330

    ApplicationDbContext Added

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
