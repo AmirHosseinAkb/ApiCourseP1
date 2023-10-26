c90075ae1ac120e7357a30320ce084dd6537ce73
AmirHossein Akbari <AkbariAmirhosseinx@gmail.com>
2023-10-26 02:19:58 -0700

    Entities Added (#2)
    * Base Entity Added To Entities
    
    * Entities Added

---
+++ Entities/Common/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace Entities.Common
+{
+
+    public interface IEntity
+    {
+
+    }
+    public class BaseEntity<TKey>:IEntity
+    {
+        public TKey Id { get; set; }
+    }
+
+    public class BaseEntity:IEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Entities/User/User.cs
@@ -0,0 +1,38 @@
+﻿using Entities.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class User : BaseEntity
+    {
+        public User()
+        {
+            IsActive = true;
+        }
+        
+        [Required]
+        [StringLength(100)]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string PasswordHash { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public GenderType Gender { get; set; }
+        public bool IsActive { get; set; }
+        public DateTimeOffset? LastLoginDate { get; set; }
+
+        public ICollection<Post> Posts { get; set; }
+    }
+
+    public enum GenderType
+    {
+        [Display(Name = "مرد")]
+        Male = 1,
+
+        [Display(Name = "زن")]
+        Female = 2
+    }
+}
