fd8be98d37fee26cf4e57bffebb45d9434475e3b
AmirHossein Akbari <AkbariAmirhosseinx@gmail.com>
2023-10-27 01:59:29 -0700

    Data Layer Primary Features Added (#3) (#4)
    * ApplicationDbContext Added
    
    * ModelBuilderExtensions Added And Configured
    
    * ApplicationDbContext Configured
    
    * Primary Tables Migration Done

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Common.Utilities;
+using Entities.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var entitiesAssembly = typeof(IEntity).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(entitiesAssembly);
+            modelBuilder.RegisterAllEntities<IEntity>(entitiesAssembly);
+            modelBuilder.AddRestrictDeleteBehaviorConvention();
+            modelBuilder.AddPluralizingTableNameConvention();
+            modelBuilder.AddGuidDefaultValueSqlConvention();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
