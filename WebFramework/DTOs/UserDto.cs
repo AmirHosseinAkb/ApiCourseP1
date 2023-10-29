5ac9ec68bcb1522deabfd37ed2af507d3bdbbbc8
AmirHossein Akbari <AkbariAmirhosseinx@gmail.com>
2023-10-29 19:56:09 +0330

    IValidatable Object Implemented In UserDto

--- WebFramework/DTOs/UserDto.cs
+++ WebFramework/DTOs/UserDto.cs
@@ -1,13 +1,22 @@
 ﻿using Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebFramework.DTOs
 {
-    public class UserDto
+    public class UserDto:IValidatableObject
     {
         public string UserName { get; set; }
         public string Password { get; set; }
         public string FullName { get; set; }
         public int Age { get; set; }
         public GenderType Gender { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (UserName.Equals("test", StringComparison.OrdinalIgnoreCase))
+                yield return new ValidationResult("The User Name Should Not Be Test");
+            if (Password == "12345678")
+                yield return new ValidationResult("Please Choose A Stronger Password");
+        }
     }
 }
