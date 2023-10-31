4fe23f2a9e6a4c16bb5ce1ecef2c96773b7d649a
AmirHossein Akbari <AkbariAmirhosseinx@gmail.com>
2023-10-31 19:56:59 +0330

    CustomExceptions Changed To Advanced

--- Common/Exceptions/AppException.cs
+++ Common/Exceptions/AppException.cs
@@ -1,12 +1,89 @@
-﻿namespace Common.Exceptions
+﻿using System.Net;
+
+namespace Common.Exceptions
 {
     public class AppException : Exception
     {
-        public ApiResultStatusCode ApiResultStatusCode { get; set; }
+        public HttpStatusCode HttpStatusCode { get; set; }
+        public ApiResultStatusCode ApiStatusCode { get; set; }
+        public object AdditionalData { get; set; }
 
-        public AppException(string message,ApiResultStatusCode apiResultStatusCode):base(message)
+        public AppException()
+            : this(ApiResultStatusCode.ServerError)
         {
-            ApiResultStatusCode=apiResultStatusCode;
+        }
+
+        public AppException(ApiResultStatusCode statusCode)
+            : this(statusCode, null)
+        {
+        }
+
+        public AppException(string message)
+            : this(ApiResultStatusCode.ServerError, message)
+        {
+        }
+
+        public AppException(ApiResultStatusCode statusCode, string message)
+            : this(statusCode, message, HttpStatusCode.InternalServerError)
+        {
+        }
+
+        public AppException(string message, object additionalData)
+            : this(ApiResultStatusCode.ServerError, message, additionalData)
+        {
+        }
+
+        public AppException(ApiResultStatusCode statusCode, object additionalData)
+            : this(statusCode, null, additionalData)
+        {
+        }
+
+        public AppException(ApiResultStatusCode statusCode, string message, object additionalData)
+            : this(statusCode, message, HttpStatusCode.InternalServerError, additionalData)
+        {
+        }
+
+        public AppException(ApiResultStatusCode statusCode, string message, HttpStatusCode httpStatusCode)
+            : this(statusCode, message, httpStatusCode, null)
+        {
+        }
+
+        public AppException(ApiResultStatusCode statusCode, string message, HttpStatusCode httpStatusCode, object additionalData)
+            : this(statusCode, message, httpStatusCode, null, additionalData)
+        {
+        }
+
+        public AppException(string message, Exception exception)
+            : this(ApiResultStatusCode.ServerError, message, exception)
+        {
+        }
+
+        public AppException(string message, Exception exception, object additionalData)
+            : this(ApiResultStatusCode.ServerError, message, exception, additionalData)
+        {
+        }
+
+        public AppException(ApiResultStatusCode statusCode, string message, Exception exception)
+            : this(statusCode, message, HttpStatusCode.InternalServerError, exception)
+        {
+        }
+
+        public AppException(ApiResultStatusCode statusCode, string message, Exception exception, object additionalData)
+            : this(statusCode, message, HttpStatusCode.InternalServerError, exception, additionalData)
+        {
+        }
+
+        public AppException(ApiResultStatusCode statusCode, string message, HttpStatusCode httpStatusCode, Exception exception)
+            : this(statusCode, message, httpStatusCode, exception, null)
+        {
+        }
+
+        public AppException(ApiResultStatusCode statusCode, string message, HttpStatusCode httpStatusCode, Exception exception, object additionalData)
+            : base(message, exception)
+        {
+            ApiStatusCode = statusCode;
+            HttpStatusCode = httpStatusCode;
+            AdditionalData = additionalData;
         }
     }
 }
--- Common/Exceptions/NotFoundException.cs
+++ Common/Exceptions/NotFoundException.cs
@@ -2,7 +2,33 @@
 {
     public class NotFoundException:AppException
     {
-        public NotFoundException(string message) : base(message, ApiResultStatusCode.NotFound)
+        public NotFoundException()
+            : base(ApiResultStatusCode.NotFound)
+        {
+        }
+
+        public NotFoundException(string message)
+            : base(ApiResultStatusCode.NotFound, message)
+        {
+        }
+
+        public NotFoundException(object additionalData)
+            : base(ApiResultStatusCode.NotFound, additionalData)
+        {
+        }
+
+        public NotFoundException(string message, object additionalData)
+            : base(ApiResultStatusCode.NotFound, message, additionalData)
+        {
+        }
+
+        public NotFoundException(string message, Exception exception)
+            : base(ApiResultStatusCode.NotFound, message, exception)
+        {
+        }
+
+        public NotFoundException(string message, Exception exception, object additionalData)
+            : base(ApiResultStatusCode.NotFound, message, exception, additionalData)
         {
         }
     }
