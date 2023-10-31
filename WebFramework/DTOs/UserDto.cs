930cb80f56616335ba2c6dcb9eb861e86ebad328
AmirHossein Akbari <AkbariAmirhosseinx@gmail.com>
2023-10-31 17:35:26 +0330

    MemberNames Parameter Added To ValidationResult In UserDto

--- WebFramework/DTOs/UserDto.cs
+++ WebFramework/DTOs/UserDto.cs
@@ -19,9 +19,9 @@ namespace WebFramework.DTOs
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             if (UserName.Equals("test", StringComparison.OrdinalIgnoreCase))
-                yield return new ValidationResult("The User Name Should Not Be Test");
+                yield return new ValidationResult("The User Name Should Not Be Test",new[] {nameof(UserName)});
             if (Password == "12345678")
-                yield return new ValidationResult("Please Choose A Stronger Password");
+                yield return new ValidationResult("Please Choose A Stronger Password", new[] {nameof(Password)});
         }
     }
 }
